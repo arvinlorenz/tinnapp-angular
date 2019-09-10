b720390b4af9ebbeaef49fab5f56912e4363b4b4
arvin lorenz <arvinlorenz@gmail.com>
2019-09-10 09:26:21 +0800

    http

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AlertController, LoadingController } from '@ionic/angular';
-import { AuthService, AuthResponseData } from './auth.service';
+import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
